7dd8271f9385458935dc06d1039e144eb6219440
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-06-30 16:46:29 +0300

    #3 снес, сделал заново проект на c#

--- LousySudoku/Sudoku/Core.cs
+++ LousySudoku/Sudoku/Core.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LousySudoku
+namespace Sudoku
 {
-    class Interface
+    public class Core
     {
     }
 }
--- LousySudoku/Sudoku/Properties/AssemblyInfo.cs
+++ LousySudoku/Sudoku/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LousySudoku")]
+[assembly: AssemblyTitle("Sudoku")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LousySudoku")]
+[assembly: AssemblyProduct("Sudoku")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c4421c2f-8c81-4c35-aec3-97b2056f4e1e")]
+[assembly: Guid("d4b32b2f-83eb-4e20-8b7f-82ba6d3908d7")]
 
 // Version information for an assembly consists of the following four values:
 //
