c74d8291d1406552c48b96ea8b445dd6ce47d9d7
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-06-30 16:40:41 +0300

    #3 изменен проект на c#
    определены классы

---
+++ LousySudoku/BlockStandart/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockStandart
+{
+    public class Debug
+    {
+        public static string TestString()
+        {
+            return "Also includes standart blocks";
+        }
+    }
+}
--- LousySudoku/LousySudoku/Core.cs
+++ LousySudoku/LousySudoku/Core.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LousySudoku
 {
     public class DBG
