10caab7d7db185e07173eeb542f7417977083546
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-06-30 19:38:37 +0300

    #3 добавлена проверка заглушки

--- lousysudoku.pas
+++ lousysudoku.pas
@@ -1,8 +1,10 @@
 {$reference 'sudoku.dll'}
 
+var the_sudoku : Sudoku.Sudoku;
 Begin
     Writeln(Sudoku.Debug.TestString());
     Sleep(5000);
     Sudoku.Debug.TestMessage();
+    the_sudoku := Sudoku.Interface.CreateSudoku();
     Readln();
 End.
