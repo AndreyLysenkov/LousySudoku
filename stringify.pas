a18067a2d959ec1bafadc27791279b1be4da8855
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-01 21:33:58 +0300

    #15 исправлены опечатки

--- stringify.pas
+++ stringify.pas
@@ -1,17 +1,13 @@
 library stringify;
 
-implementation
-
 procedure ToArray(value : string; var output : array of integer);
 begin
 
 end;
 
-procedure FromArray(var value : string; value : array of integer);
+procedure FromArray(var value : string; array_input : array of integer);
 begin
 
 end;
 
-initialization
-
-End.
+end.
