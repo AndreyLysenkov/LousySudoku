5046425ffe0bfb2b4ff212a76eb904e58dfa990f
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-01 11:54:49 +0300

    #8 изменена структура

--- LousySudoku/Sudoku/Block.cs
+++ LousySudoku/Sudoku/Block.cs
@@ -35,22 +35,15 @@ namespace Sudoku
             return result;
         }
 
-        public virtual int[] Check()
+        protected virtual int[] Check(int[] value, bool[] mask)
         {
-            int[] result = new int[0];
-            for (int i = 0; i < children.Length - 1; i++)
-            {
-                for (int j = i + 1; j < children.Length; j++)
-                {
-                    if (children[i].Value == children[j].Value)
-                    {
-                        Array.Resize(ref result, result.Length + 2);
-                        result[result.Length - 1] = i;
-                        result[result.Length - 2] = j;
-                    }
-                }
-            }
-            return result;
+
+            return null;
+        }
+
+        public int[] Check()
+        {
+            return Check(this.GetValues(), this.GetValuesMask());
         }
 
         
