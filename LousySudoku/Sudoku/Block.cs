070268c3037f34ab048bd7cc2cd082355128e29b
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-01 11:33:11 +0300

    #8 функция создана набросок

--- LousySudoku/Sudoku/Block.cs
+++ LousySudoku/Sudoku/Block.cs
@@ -18,7 +18,18 @@ namespace Sudoku
         public virtual int[] Check()
         {
             int[] result = new int[0];
-
+            for (int i = 0; i < children.Length - 1; i++)
+            {
+                for (int j = i + 1; j < children.Length; j++)
+                {
+                    if (children[i].value == children[j].value)
+                    {
+                        Array.Resize(ref result, result.Length + 2);
+                        result[result.Length - 1] = i;
+                        result[result.Length - 2] = j;
+                    }
+                }
+            }
             return result;
         }
 
