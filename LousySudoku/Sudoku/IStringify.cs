dc3396c92f95242d782e78dfa309e199549a962c
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-01 16:04:13 +0300

    #12 добавлен интерфейс IStringify

---
+++ LousySudoku/Sudoku/IStringify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    public interface IStringify
+    {
+
+        string Stringify();
+
+        static IStringify Unstringify(string value);
+
+    }
+}
