2cca92340e4b23e3ffd6fdbcec8fa9530bf2506a
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-01 21:30:54 +0300

    #15 add заглушки для методов

---
+++ stringify.pas
@@ -0,0 +1,17 @@
+library stringify;
+
+implementation
+
+procedure ToArray(value : string; var output : array of integer);
+begin
+
+end;
+
+procedure FromArray(var value : string; value : array of integer);
+begin
+
+end;
+
+initialization
+
+End.
