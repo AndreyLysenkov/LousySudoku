73f1b72db90387881e122b6a55839a0065f9c816
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-04 01:10:42 +0300

    #21 add метод вывода судоку

--- LousySudoku/Sudoku/Debug.cs
+++ LousySudoku/Sudoku/Debug.cs
@@ -17,13 +17,25 @@ namespace Sudoku
             Console.Write("This just for... reasons unknown");
         }
 
+        public static void ShowSudoku(Sudoku sudoku, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(sudoku.ReturnNumberByPosition(new Number.Position(i, j)).Value);
+                }
+                Console.WriteLine();
+            }
+        }
+
         public static void TestSudoku1()
         {
             int[,] value = new int[9, 9];
             Number.NumberType[,] mask = new Number.NumberType[9, 9];
             Number.Position[,] block = new Number.Position[9, 9];
             Sudoku sudoku = new Sudoku(value, mask, block);
-
+            ShowSudoku(sudoku, 9);
         }
     }
 }
