c7d1a1fab7710ecdb28a9d2ea347613fb7f93396
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-04 20:22:33 +0300

    #10 correct описание блоков для 9x9

--- SudokuWPFInterface/WpfApplication1/SudokuBuilder.cs
+++ SudokuWPFInterface/WpfApplication1/SudokuBuilder.cs
@@ -13,27 +13,50 @@ namespace WpfApplication1
         {
 
 
-            Number.Position[,] block = new Number.Position[9 + 9 , 9];
+            Number.Position[][] block = new Number.Position[9 + 9][];
 
             ///Добавление блоков (горизонтальные линии);
             for (int i = 0; i < 9; i++)
             {
+                block[i] = new Number.Position[9];
                 for (int j = 0; j < 9; j++)
                 {
-                    block[i, j] = new Number.Position(i, j);
+                    block[i][j] = new Number.Position(i, j);
                 }
             }
 
             ///Добавление блоков (вертикальные линии);
             for (int i = 0; i < 9; i++)
             {
+                block[9 + i] = new Number.Position[9];
                 for (int j = 0; j < 9; j++)
                 {
-                    block[9 + i, j] = new Number.Position(j, i);
+                    block[9 + i][j] = new Number.Position(j, i);
                 }
             }
 
-            return Sudoku.Debug.TestSudoku1();
+            int[,] value = new int[9, 9];
+            //////////////
+            Random rand = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    value[i, j] = rand.Next(9);
+                }
+            }
+            ////////////////
+
+            Number.NumberType[,] mask = new Number.NumberType[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    mask[i, j] = Number.NumberType.Modify;
+                }
+            }
+
+            return new Sudoku.Sudoku(new Number.Position(9, 9), value, mask, block);
         }
     }
 }
