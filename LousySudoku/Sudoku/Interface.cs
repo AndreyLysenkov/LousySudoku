91f5f15114a873ee672feacdef665728e23df7f7
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-05 16:22:02 +0300

    #31 add шаблон для судоку 9x9

--- LousySudoku/Sudoku/Interface.cs
+++ LousySudoku/Sudoku/Interface.cs
@@ -18,5 +18,58 @@ namespace Sudoku
             return sudoku.ChangeNumber(number_position, value);
         }
 
+        public static class SudokuBuilder
+        {
+
+            public static Sudoku GetStandart9(int[,] numbs)
+            {
+
+                Number.Position[][] block = new Number.Position[9 + 9][];
+
+                ///Добавление блоков (горизонтальные линии);
+                for (int i = 0; i < 9; i++)
+                {
+                    block[i] = new Number.Position[9];
+                    for (int j = 0; j < 9; j++)
+                    {
+                        block[i][j] = new Number.Position(i, j);
+                    }
+                }
+
+                ///Добавление блоков (вертикальные линии);
+                for (int i = 0; i < 9; i++)
+                {
+                    block[9 + i] = new Number.Position[9];
+                    for (int j = 0; j < 9; j++)
+                    {
+                        block[9 + i][j] = new Number.Position(j, i);
+                    }
+                }
+
+                int[,] value = new int[9, 9];
+                //////////////
+                Random rand = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        value[i, j] = rand.Next(9);
+                    }
+                }
+                ////////////////
+
+                Number.NumberType[,] mask = new Number.NumberType[9, 9];
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        mask[i, j] = Number.NumberType.Modify;
+                    }
+                }
+
+                return new Sudoku(new Number.Position(9, 9), value, mask, block);
+            }
+        }
+
     }
 }
