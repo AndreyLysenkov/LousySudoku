217580bd8ed9b1b07e8c043e78c83a19f265d80b
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-06 21:35:41 +0300

    #36 add sudoku 9x9 template method

--- LousySudoku/Sudoku/Debug.cs
+++ LousySudoku/Sudoku/Debug.cs
@@ -125,6 +125,73 @@ namespace LousySudoku
             return result;
         }
 
+        public static Sudoku GetStandart9(int[,] numbs)
+        {
+
+
+            Number.Position[][] block = new Number.Position[9 + 9 + 9][];
+
+            ///Добавление блоков (горизонтальные линии);
+            for (int i = 0; i < 9; i++)
+            {
+                block[i] = new Number.Position[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    block[i][j] = new Number.Position(i, j);
+                }
+            }
+
+            ///Добавление блоков (вертикальные линии);
+            for (int i = 0; i < 9; i++)
+            {
+                block[9 + i] = new Number.Position[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    block[9 + i][j] = new Number.Position(j, i);
+                }
+            }
+
+            ///Добавление блоков 3x3;
+            int blockIndex = 9 + 9;
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3, blockIndex++)
+                {
+                    block[blockIndex] = new Number.Position[9];
+                    int cellIndex = 0;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++, cellIndex++)
+                        {
+                            block[blockIndex][cellIndex] = new Number.Position(i + k, j + l);
+                        }
+                    }
+                }
+            }
+
+            int[,] value = new int[9, 9];
+            //////////////
+            Random rand = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    value[i, j] = rand.Next(9);
+                }
+            }
+            ////////////////
+
+            Number.NumberType[,] mask = new Number.NumberType[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    mask[i, j] = Number.NumberType.Empty;
+                }
+            }
+
+            return new Sudoku(new Number.Position(9, 9), value, mask, block, 9);
+        }
     }
 
 }
