65d21ffb5f89f87000c6c54ae4d0c2f82ceb2e66
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-07 22:33:15 +0300

    #41 add external check method to pascal dll file

--- data/block/standart.pas
+++ data/block/standart.pas
@@ -1,8 +1,21 @@
 library standart;
 
-procedure Check(number : array of integer; var indexes : array of integer);
+function Check(value : array of integer; var mask : array of boolean) : array of integer;
 begin
-
+    SetLength(result, 0);
+    for var i : integer := 0 to value.Length - 1 do
+    begin
+        for var j : integer := i + 1 to value.Length - 1 do
+        begin
+            if ((((mask[i]) and (mask[j])) and (value[i] = value[j]))) then
+                begin
+                    System.Array.Resize(result, result.Length + 2);
+                    result[result.Length - 1] := i;
+                    result[result.Length - 2] := j;
+                end;
+        end;    
+    end;
+    Check := result;
 end;
 
 end.
