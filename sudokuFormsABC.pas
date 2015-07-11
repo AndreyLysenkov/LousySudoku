9d5e046fd3340a7dc085f997e06af4ccaeef0d23
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-11 14:38:39 +0300

    #16 add fields for file chooing

--- sudokuFormsABC.pas
+++ sudokuFormsABC.pas
@@ -9,11 +9,38 @@ const
     windowTitle : string = 'LousySudoku v1';
 var
     theSudoku : Sudoku;
+function PutTextBox(boxLabel : string; boxText : string; boxHeight : integer; boxWidth : integer) : TextBox;
+begin
+    var thLabel : TextLabel := new TextLabel(boxLabel);
+    var textBox : TextBox := new TextBox();
+    textBox.Text := boxText;
+    textBox.Height := boxHeight;
+    textBox.Width := boxWidth;
+    Result := textBox;
+end;
+
+procedure OpenFile(obj : object);
+begin
+    
+end;
+
 procedure FileChooseWindow();
 begin
     MainForm.IsFixedSize := true;
     MainForm.Height := 150;
     MainForm.Width := 400;    
+    filenameTextBox := PutTextBox(
+            'Template filename: ', 
+            directory + extension,
+            24,
+            270
+        );
+    complexityTextBox := PutTextBox(
+            'Complexity [0-1]: ', 
+            complexityDefault.ToString(),
+            24,
+            50
+        );
     var buttonOk : Button := new Button('Generate sudoku');        
 end;
 
