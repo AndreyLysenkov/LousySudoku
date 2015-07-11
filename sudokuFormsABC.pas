00aff0ce6cffb1e49dca81e1c167b0615a7ff396
Andrey Lysenkov <lysenkov-ae@ya.ru>
2015-07-11 14:38:17 +0300

    #16 add File choice procedure

--- sudokuFormsABC.pas
+++ sudokuFormsABC.pas
@@ -9,6 +9,15 @@ const
     windowTitle : string = 'LousySudoku v1';
 var
     theSudoku : Sudoku;
+procedure FileChooseWindow();
+begin
+    MainForm.IsFixedSize := true;
+    MainForm.Height := 150;
+    MainForm.Width := 400;    
+    var buttonOk : Button := new Button('Generate sudoku');        
+end;
+
 begin
     MainForm.Title := windowTitle;
+    FileChooseWindow();
 end.
